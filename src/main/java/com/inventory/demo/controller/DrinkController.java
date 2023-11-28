7e49d4d0bc041a623d3f5763282e8869c9ff2c77
dantheman <danogbo0@gmail.com>
2023-11-28 12:54:41 +0100

    recent commit

--- src/main/java/com/inventory/demo/controller/DrinkController.java
+++ src/main/java/com/inventory/demo/controller/DrinkController.java
@@ -1,4 +1,38 @@
 package com.inventory.demo.controller;
 
+
+import com.inventory.demo.domain.Drink;
+import com.inventory.demo.services.DrinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+
 public class DrinkController {
+    @Autowired
+    DrinkService drinkService;
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("drinks", drinkService.getAllDrink());
+        return "home";
+    }
+
+    @GetMapping("/showNewDrinkForm")
+    public String showNewDrinkForm(Model model){
+        Drink drink = new Drink();
+        model.addAttribute("drink", drink);
+
+        return "new_drink";
+    }
+
+    @PostMapping("/saveDrink")
+    public String saveDrink(@ModelAttribute("drink") Drink drink){
+        drinkService.saveDrink(drink);
+        return "redirect:/";
+    }
 }
