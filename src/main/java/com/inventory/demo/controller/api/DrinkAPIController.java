53af60d526b3b4ee6ed6d1a38e71aa08dc9014b4
dantheman <danogbo0@gmail.com>
2023-11-28 10:38:26 +0100

    recent fix commit

--- src/main/java/com/inventory/demo/controller/api/DrinkAPIController.java
+++ src/main/java/com/inventory/demo/controller/api/DrinkAPIController.java
@@ -2,6 +2,7 @@ package com.inventory.demo.controller.api;
 
 import com.inventory.demo.domain.Drink;
 import com.inventory.demo.repository.DrinkRepository;
+import com.inventory.demo.services.DrinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,7 +16,7 @@ public class DrinkAPIController {
     DrinkRepository drinkRepository;
 
     @Autowired
-    DrinkRepository drinkService;
+    DrinkService drinkService;
 
     @RequestMapping("/drinks")
     @GetMapping("/drinks")
@@ -31,11 +32,11 @@ public class DrinkAPIController {
 
     @PostMapping("/drink")
     public ResponseEntity<Drink> createDrink(@RequestBody Drink drink){
-        return ResponseEntity.ok().body(drinkService.saveDrink(id));
+        return ResponseEntity.ok().body(drinkService.saveDrink(drink));
     }
 
     @PutMapping("/drink/{id}")
     public ResponseEntity<Drink> updateDrink(@PathVariable long id, @RequestBody Drink drink){
-        return ResponseEntity.ok().body(drinkService.updateDrink(drink));
+        return ResponseEntity.ok().body(drinkService.updateDrinkById(drink));
     }
 }
